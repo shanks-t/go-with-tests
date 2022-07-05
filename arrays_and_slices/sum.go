3ab2348317865a1dbf3f1cea4c209448ab5c6129
shanks-t <shanksleslietrey@gmail.com>
2022-07-05 18:39:04 -0500

    added test for arrays and slices, introduced 'go tets -cover'

---
+++ arrays_and_slices/sum.go
@@ -0,0 +1,13 @@
+package arraysandslices
+
+func Sum(numbers []int) int {
+	sum := 0
+	// for i := 0; i < 5; i++ {
+	// 	sum += numbers[i]
+	// }
+
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
