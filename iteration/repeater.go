3a1aca4f0d33c6d06b697bf53902b72934d9ae7a
shanks-t <shanksleslietrey@gmail.com>
2022-07-05 17:41:44 -0500

    added for loop test with benchmark

---
+++ iteration/repeater.go
@@ -0,0 +1,10 @@
+package iteration
+
+// takes a string and repeats it n times and returns repeated string
+func Repeat(character string, repeatCount int) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
