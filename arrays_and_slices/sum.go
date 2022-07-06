78b3471aba6df84b2d1dac73f7564dc885d066eb
shanks-t <shanksleslietrey@gmail.com>
2022-07-06 14:39:49 -0500

    added test to sum tails of slices, extracted assertions into checksums function to make test more dry, which adds type-safety to tests

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -11,3 +11,32 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
