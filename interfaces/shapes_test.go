b3153f45512c6b3a68d4bf52460b902509b30ee1
shanks-t <shanksleslietrey@gmail.com>
2022-07-06 16:37:29 -0500

    made test more descriptive by name 'want' field 'hasArea', added %#v to error string to provide name of properties, added 't.run' to pring name of test cases

--- interfaces/shapes_test.go
+++ interfaces/shapes_test.go
@@ -15,19 +15,23 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 10, Height: 5}, want: 25.0},
+		{shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 10, Height: 5}, hasArea: 25.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		// using the tt.name from the case to use it as the 't.run' test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 
 }
