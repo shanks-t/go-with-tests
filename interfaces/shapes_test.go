dd1c538af563a65beb725828b0c5fbff4f49e040
shanks-t <shanksleslietrey@gmail.com>
2022-07-06 16:28:28 -0500

    added name fields for structs in table test's slice of structs

--- interfaces/shapes_test.go
+++ interfaces/shapes_test.go
@@ -18,9 +18,9 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{10, 5}, 25.0},
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 10, Height: 5}, want: 25.0},
 	}
 
 	for _, tt := range areaTests {
