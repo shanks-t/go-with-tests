8eaf7d6db1e8f19768780e647639782c0b35e1d8
shanks-t <shanksleslietrey@gmail.com>
2022-07-06 16:13:47 -0500

    added types to handle calculating area for different shapes, added interface for shapes to decouple helper function from concrete types

---
+++ interfaces/shapes.go
@@ -0,0 +1,32 @@
+package interfaces
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// func Area(rectangle Rectangle) float64 {
+// 	return rectangle.Width * rectangle.Height
+// }
