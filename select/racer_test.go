431106ca81a9b937a54dc8af0a1a1c3064cddbb1
shanks-t <shanksleslietrey@gmail.com>
2022-07-10 14:35:51 -0500

    added configurable racer, so racer used config-racer under hood which allows us to set a timeout in racer and use shorter timeouts in racer_test, making the test much faster

--- select/racer_test.go
+++ select/racer_test.go
@@ -8,22 +8,40 @@ import (
 )
 
 func TestRacer(t *testing.T) {
+	t.Run("compares speeds of servers, returning fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("returns an error if server does not respond in specified time", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Fatalf("expected and error but didn't get one")
+		}
+
+	})
+
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
