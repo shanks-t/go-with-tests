fd3abcf6030ebb8bc4c2a2a3ba54b80e9a8de342
shanks-t <shanksleslietrey@gmail.com>
2022-08-05 10:13:41 -0500

    removed commented out code from other program

--- dependency_injection/di_NCCI.go
+++ dependency_injection/di_NCCI.go
@@ -6,35 +6,6 @@ import (
 	"time"
 )
 
-// func homeHandler(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-// 	fmt.Fprint(w, "<h1>Welcom to mah dope site!</h1>")
-// }
-
-// func contactHandler(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-// 	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a
-//     href=\"mailto:shanksleslietrey@gmail.com\">shanksleslietrey@gmail.com</a>.`)
-// }
-
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		w.WriteHeader(http.StatusNotFound)
-// 		fmt.Fprint(w, "Page not found")
-// 	}
-// }
-
-// func main() {
-// 	http.HandleFunc("/", pathHandler)
-// 	fmt.Println("Starting server on :3000...")
-// 	http.ListenAndServe(":3000", nil)
-// }
-
 type Interview interface {
 	askQuestion() []string
 	getDifficulty(index int) int
