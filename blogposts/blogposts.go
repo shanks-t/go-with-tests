810a5b6e121db9cbee0955082c50b6bb585de642
shanks-t <shanksleslietrey@gmail.com>
2022-08-07 13:49:01 -0500

    extracted getPost function in refactore

---
+++ blogposts/blogposts.go
@@ -0,0 +1,42 @@
+package blogposts
+
+import (
+	"io"
+	"io/fs"
+)
+
+type Post struct {
+	Title string
+}
+
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			return nil, err //todo: needs clarification should we fail totally if one file fails, or ignor
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
+}
