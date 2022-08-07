0b70f8f7777f1f7723e209f20a299612dc419874
shanks-t <shanksleslietrey@gmail.com>
2022-08-07 10:18:55 -0500

    unsure how to mock the random number gen feature of program

--- dependency_injection/di_NCCI_test.go
+++ dependency_injection/di_NCCI_test.go
@@ -2,10 +2,26 @@ package main
 
 import "testing"
 
+var MockInterviewer1 = Interviewer{
+	name: "Ovidio", interviewQuestions: []Question{
+		{
+			questionString: "Explain dependency injection",
+			difficulty:     9,
+		},
+		{
+			questionString: "Explain polymorphism",
+			difficulty:     8,
+		},
+	},
+}
+
 func ConductInterviewTest(t *testing.T) {
+	mockInterviewers := []Interview{MockInterviewer1}
+	got := ConductInterview(mockInterviewers)
+	want := 
 
 }
 
 func BenchmarkConductInterview(b *testing.B) {
-	main()
+	ConductInterview()
 }
