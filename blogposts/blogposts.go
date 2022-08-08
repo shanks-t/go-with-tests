245b66129ed4099dd8460178bff9c5a8d566cab6
shanks-t <shanksleslietrey@gmail.com>
2022-08-08 09:54:36 -0500

    extracted newPost method, refactored test with helper assertion, added constants to test, refactored get NewPostFromFS, getPost to use file name string, newPost to use io.Reader interface and bufio.NewScanner to read props on post struct

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -1,12 +1,14 @@
 package blogposts
 
 import (
+	"bufio"
 	"io"
 	"io/fs"
 )
 
 type Post struct {
-	Title string
+	Title       string
+	Description string
 }
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
@@ -16,7 +18,7 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	}
 	var posts []Post
 	for _, f := range dir {
-		post, err := getPost(fileSystem, f)
+		post, err := getPost(fileSystem, f.Name())
 		if err != nil {
 			return nil, err //todo: needs clarification should we fail totally if one file fails, or ignor
 		}
@@ -25,18 +27,23 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	return posts, nil
 }
 
-func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
-	postFile, err := fileSystem.Open(f.Name())
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
 	if err != nil {
 		return Post{}, err
 	}
 	defer postFile.Close()
+	return newPost(postFile)
+}
 
-	postData, err := io.ReadAll(postFile)
-	if err != nil {
-		return Post{}, err
-	}
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
 
-	post := Post{Title: string(postData)[7:]}
-	return post, nil
+	scanner.Scan()
+	titleLine := scanner.Text()
+
+	scanner.Scan()
+	descriptionLine := scanner.Text()
+
+	return Post{Title: titleLine[7:], Description: descriptionLine[13:]}, nil
 }
