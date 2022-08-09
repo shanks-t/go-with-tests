eac6e043d28c8819a0287ee676fecd3887285575
shanks-t <shanksleslietrey@gmail.com>
2022-08-09 16:41:35 -0500

    got test to pass, the refactored to use template string with html/template package parse and execute methods

---
+++ blogrenderer/blogRenderer_test.go
@@ -0,0 +1,35 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/shanks-t/go-with-tests/blogrenderer"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogrenderer.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
+}
